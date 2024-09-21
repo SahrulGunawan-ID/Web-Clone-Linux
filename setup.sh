64769fe0bae0318d54b0602ad13d1c579591c112
Sahrul Gunawan Cyber <developer.limited.id@gmail.com>
2024-09-21 11:35:36 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# Author : Sahrul Gunawan Cyber
+# Youtube Channel : https://youtube.com/@bangtutorialofficial1152
+# Github ID : https://github.com/SahrulGunawan-ID/Web-Clone-Linux
+
+clear
+apt update && apt upgrade -y
+apt install openssl openssh wget wget2 curl bash -y
+clear
+
+apt install zip
+unzip webclone.zip
+sleep 2
+clear
+ls
+clear
+# Start Script Now
+sh webclone.sh
